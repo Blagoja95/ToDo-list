52032296a3e5b233379587faed7a8d32664aae51
Boris <boris.blagojevicc@hotmail.com>
2022-03-17 11:58:12 +0100

    Submit when enter is pressed, fix for input value

--- script/toDoList.js
+++ script/toDoList.js
@@ -1,13 +1,12 @@
-
-const input = document.querySelector('.textArea');
-const submitBtn = document.querySelector('.btn-submit');
-const tasks = document.querySelector('.tasks');
-const content = document.querySelector('.content');
-const h2 = document.querySelector('.content h2');
-const msg = document.querySelector('.message')
+const input = document.querySelector(".textArea");
+const submitBtn = document.querySelector(".btn-submit");
+const tasks = document.querySelector(".tasks");
+const content = document.querySelector(".content");
+const h2 = document.querySelector(".content h2");
+const msg = document.querySelector(".message");
 
 // put placeholder text in iput text area
-input.setAttribute('placeholder', 'Buy eggs!');
+input.setAttribute("placeholder", "Buy eggs!");
 
 let btnEditArr = [];
 
@@ -15,87 +14,90 @@ let str = [];
 let taskCounter = 0;
 let check = true;
 
-submitBtn.addEventListener('click', function () {
+const addTask = function () {
+  // remove msg if it is displeyd
+  msg.classList.add("hidden");
 
-    // remove msg if it is displeyd
-    msg.classList.add('hidden');
+  if (input.value !== "" && check == true) {
+    str[taskCounter] = input.value;
+    input.value = "";
+    // console.log(str[taskCounter]);
 
+    // Creating ToDo element
+    //create task
+    const task = document.createElement("div");
+    task.setAttribute("class", "task");
 
-    if (input.value !== '' && check == true) {
-        str[taskCounter] = input.value;
-        // console.log(str[taskCounter]);
+    //ToDO text
+    const content = document.createElement("p");
+    content.setAttribute("class", "task-content");
+    task.appendChild(content);
+    content.innerHTML = str[taskCounter];
 
-        // Creating ToDo element
-        //create task
-        const task = document.createElement('div');
-        task.setAttribute('class', 'task');
+    //ToDO buttnons
+    //div
+    const btnTask = document.createElement("div");
+    btnTask.setAttribute("class", "btn-tasks");
+    task.appendChild(btnTask);
 
-        //ToDO text
-        const content = document.createElement('p');
-        content.setAttribute('class', 'task-content');
-        task.appendChild(content);
-        content.innerHTML = str[taskCounter];
+    //edit
+    const btnEdit = document.createElement("button");
+    btnEdit.innerHTML = "Edit";
+    btnEdit.setAttribute("class", "btn-edit");
 
-        //ToDO buttnons
-        //div
-        const btnTask = document.createElement('div');
-        btnTask.setAttribute('class', 'btn-tasks');
-        task.appendChild(btnTask);
+    //edit task event
+    btnEdit.addEventListener("click", function () {
+      // first i change elemets for edit mode
+      h2.innerHTML = "Edite your task";
+      submitBtn.value = "Edit";
+      input.value = content.innerHTML; // take value from task that is selected
 
-        //edit
-        const btnEdit = document.createElement('button');
-        btnEdit.innerHTML = "Edit";
-        btnEdit.setAttribute('class', 'btn-edit');
+      // to disable submit task mode
+      check = false;
 
-        //edit task event
-        btnEdit.addEventListener('click', function () {
-            // first i change elemets for edit mode
-            h2.innerHTML = 'Edite your task';
-            submitBtn.value = 'Edit';
-            input.value = content.innerHTML; // take value from task that is selected
+      //edit button in edit mode
+      submitBtn.addEventListener("click", function () {
+        //get changed name for selected task
+        content.innerHTML = input.value;
 
-            // to disable submit task mode
-            check = false;
+        //get back everything to submit mode
+        h2.innerHTML = "Add Items";
+        submitBtn.value = "Submit";
+        input.value = "";
 
-            //edit button in edit mode
-            submitBtn.addEventListener('click', function () {
+        // msg to user that edit task is complited
+        msg.classList.remove("hidden");
+        check = true; // back to submit mode
+      });
+    });
 
-                //get changed name for selected task
-                content.innerHTML = input.value;
+    btnTask.appendChild(btnEdit);
+    btnEditArr[taskCounter] = btnTask;
 
-                //get back everything to submit mode
-                h2.innerHTML = 'Add Items';
-                submitBtn.value = 'Submit';
-                input.value = '';
+    //delete
+    const btnDel = document.createElement("button");
+    btnDel.innerHTML = "Delete";
+    btnDel.setAttribute("class", "btn-del");
 
-                // msg to user that edit task is complited 
-                msg.classList.remove('hidden');
-                check = true; // back to submit mode
-            })
-        })
+    //remove task event
+    btnDel.addEventListener("click", function () {
+      tasks.removeChild(task);
+    });
+    btnTask.appendChild(btnDel);
 
-        btnTask.appendChild(btnEdit);
-        btnEditArr[taskCounter] = btnTask;
+    // Append a whole ToDo element
+    const append = tasks.appendChild(task);
+    taskCounter++;
 
-        //delete 
-        const btnDel = document.createElement('button');
-        btnDel.innerHTML = "Delete";
-        btnDel.setAttribute('class', 'btn-del');
-
-        //remove task event
-        btnDel.addEventListener('click', function () {
-            tasks.removeChild(task);
-        })
-        btnTask.appendChild(btnDel);
-
-        // Append a whole ToDo element 
-        const append = tasks.appendChild(task);
-        taskCounter++;
-
-        // add task heading
-        if (taskCounter > 0) {
-            document.querySelector('.tasks h2').classList.remove('hidden');
-        }
+    // add task heading
+    if (taskCounter > 0) {
+      document.querySelector(".tasks h2").classList.remove("hidden");
     }
-})
+  }
+};
 
+// Submit
+submitBtn.addEventListener("click", addTask);
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") addTask();
+});
